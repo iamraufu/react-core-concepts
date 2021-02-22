95e3a4d6676870ab0a038536ddc4409c6ef7c648
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-23 01:43:56 +0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    var person = {
+        name: "Raufu Prezens",
+        job: "Programmer"
+    }
+    var person2 = {
+        name: "Eftykhar Rahman",
+        job: "Software Engineer"
+    }
+    var style = {
+        color: 'lightblue',
+        backgroundColor: "yellow"
+    }
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        p >
+        Edit Done < code > src / App.js < /code> and save to reload. < /
+        p >
+        <
+        h1 className = ""
+        style = { style } > { person.name + " " + person.job } < /h1> <
+        h2 style = {
+            {
+                color: 'cyan',
+                backgroundColor: "tomato"
+            }
+        } > { person2.name + " " + person2.job } < /h2> <
+        p > My First React App < /p> < /
+        header > <
+        /div>
+    );
 }
 
 export default App;
